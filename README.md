344fb0ec3309a540786b2431bcf22fd6be6ba3f3
rodrigoarias <rodrigo.arias@uala.com.ar>
2022-09-20 15:10:45 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # changelog-from-pr-file
-Generates a changelog from the repository PR local files
+Generates a changelog from the repository PR local files.
+A very (very) specific actions intended to be used with [pr-to-file-action](https://github.com/rodrigoarias/pr-to-file-action).
+That action creates a file for every PR. This action use those files and:
+- Generates a changelog text.
+- Updates the version name (according to the number and type of PRs).
+
+This was created for a specific project so it assumes a lot of things:
+- There is a root folder named `changes` with a file for every PR named `PR-number.json`
+- That file has a json with the fields name, number and url.
+- Those names could start with `fix:`, `feat:` and other [Conventional Commits prefixes](https://www.conventionalcommits.org/en/v1.0.0/)
+- The input version name is in the format `major.minor.patch`
+
+
+## Inputs
+
+## `git-token`
+
+**Required** A GITHUB_SECRET with enough permissions to read files.
+
+## `current-version`
+
+The name of the version to be updated, in format `major.minor.patch`. Default: `"0.0.0"`
+
+
+## Outputs
+
+## `changelog`
+
+The generated changelog.
+
+## `version`
+
+The input version updated by the number and type of changes.
+
+## Example usage
+```
+    # Generate changelog
+    - name: Generate changelog
+      id: changelog
+      uses: rodrigoarias/changelog-from-pr-file@v1.0.0
+      with:
+        git-token: ${{ secrets.RELEASE_GITHUB_KEY }}
+        current-version: "1.0.0"
+      
+```
---
+++ index.js
@@ -0,0 +1,159 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+var output
+
+var patchChanges = 0
+var minorChanges = 0
+var majorChanges = 0
+
+var patchVersion = 0
+var minorVersion = 0
+var majorVersion = 0
+
+
+const fixPrefix = "fix:"
+const featPrefix = "feat:"
+const chorePrefix = "chore:"
+const docsPrefix = "docs:"
+const refactorPrefix = "refactor:"
+const stylePrefix = "style:"
+const testPrefix = "test:"
+
+const features = []
+const fixes = []
+const others = []
+
+
+
+const main = async (workspace) => {
+  const myToken = core.getInput('git-token');
+  const octokit = github.getOctokit(myToken);
+  const versionName = core.getInput('current-version');
+
+  try {
+    const subversions = versionName.split('.');
+    majorVersion = Number(subversions[0]);
+    minorVersion = Number(subversions[1]);
+    patchVersion = Number(subversions[2]);
+  } catch (error) {
+    console.log(`error ${error}`);
+  }
+
+
+  const owner = github.context.payload.repository.owner.login
+  const repo = github.context.payload.repository.name
+
+  const changeFiles = await findFile(octokit, owner, repo, '/changes');
+
+  await processAllFiles(changeFiles, octokit, owner, repo, '/changes');
+  majorVersion += majorChanges;
+  minorVersion += minorChanges;
+  patchVersion += patchChanges;
+
+  createOutputFromChanges();
+  console.log(`${output}`)
+  core.setOutput("changelog", output);
+  core.setOutput("version", `${majorVersion}.${minorVersion}.${patchVersion}`);
+}
+
+const createOutputFromChanges = () => {
+  output = `## v ${majorVersion}.${minorVersion}.${patchVersion}`
+  output += "\n"
+
+  output = addSection(output, features, '### 🚀 Features');
+  output = addSection(output, fixes, '### 🐛 Bugs');
+  output = addSection(output, others, '### 📝 Others');
+  return output;
+
+}
+
+const addSection = (output, changes, title) => {
+  if (!changes.length) {
+    return;
+  }
+  output += title;
+  output += '\n';
+  for (const log of changes) {
+    output += log;
+    output += '\n';
+  }
+  return output;
+}
+
+const processAllFiles = async (files, octokit, organization, repo, fileName) => {
+  if (Array.isArray(files)) {
+    for (const element of files) {
+      await lookForPRFile(octokit, organization, repo, element.path)
+    }  
+  } else {
+    console.log('Not a valid directory (folder)')
+  }
+}
+ 
+
+const lookForPRFile = async(octokit, organization, repo, fileName) => {
+    const file = await findFile(octokit, organization, repo, fileName)
+    const buff = Buffer.from(file.content, 'base64');
+    const content = buff.toString('ascii');
+    const pr = JSON.parse(content);
+
+    
+    processTypeOfChange(pr);
+}
+
+const processTypeOfChange = (pr) => {
+  const changeMessage = pr.title;
+  const prefix = ''
+
+  switch (true) {
+  case changeMessage.startsWith(chorePrefix):
+    processChange(pr, others, chorePrefix);
+    break;
+  case changeMessage.startsWith(docsPrefix):
+    processChange(pr, others, docsPrefix);
+    break;
+  case changeMessage.startsWith(stylePrefix):
+    processChange(pr, others, stylePrefix);
+    break;
+  case changeMessage.startsWith(testPrefix):
+    processChange(pr, others, testPrefix);
+    break;
+
+  case changeMessage.startsWith(featPrefix):
+    processChange(pr, features, featPrefix);
+    minorChanges++;
+    break;
+    
+  case changeMessage.startsWith(refactorPrefix):
+    processChange(pr, fixes, refactorPrefix);
+    patchChanges++;
+    break;
+
+  case changeMessage.startsWith(fixPrefix):
+    processChange(pr, fixes, fixPrefix);
+    patchChanges++;
+    break;
+
+  default:
+    processChange(pr, fixes, '');
+    patchChanges++;
+    break;
+  }
+}
+
+const processChange = (pr, sameLevelChanges, prefix) => {
+  var line = pr.title.replace(prefix, '')
+  line += " " + "[PR-" + pr.number + "](" + pr.url + ")";
+  sameLevelChanges.push(line);
+}
+
+const findFile = async(octokit, organization, repo, fileName, branch) => {
+  gitFile = await octokit.rest.repos.getContent({
+    owner: organization,
+    repo: repo,
+    path: fileName,
+  })
+  return gitFile.data;
+}
+
+main()
